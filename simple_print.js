8a0e0d11ce41387f0dfe89ae360aaface6f61f73
Tabitha Ama Abeka <99736998+a-tae-bi@users.noreply.github.com>
2022-06-24 10:16:38 +0000

    Printed my name, height and location to the screen
    Wrote a simple JavaScript code that prints out your name, height, and country on the screen.

---
+++ simple_print.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+
+console.log('Tabitha Ama Abeka')
+
+console.log('Height: 163cm');
+
+console.log('Country: Ghana');
